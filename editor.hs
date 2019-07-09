5bef692ed9c5fcb7856c866a29ae03cda1176137
Daniel Turner <35703802+turnerdaniel@users.noreply.github.com>
2019-07-09 14:05:04 +0100

    Added help system

--- editor.hs
+++ editor.hs
@@ -143,10 +143,50 @@ removeSpace text
     | (head text) == ' ' = removeSpace(tail text)
     | otherwise = (head text) : removeSpace (tail text)
 
+-- Display help information
 help = putStrLn "Commands available for the Text Editor:\n\
                 \\n\
-                \   <statement>    execute <statement>\n\
-                \   create         Create a Text Editor instance\n\
-                \   ..."
-
---TODO: Add help text + examples 
+                \   create                            Create a new Text Editor.\n\
+                \   initialise <TextEditor> <Text>    Initialise the Text Editor with some text.\n\
+                \   destroy <TextEditor>              Delete the Text Editor.\n\
+                \   moveLeft <TextEditor>             Move the cursor one position to the left.\n\
+                \   moveRight <TextEditor>            Move the cursor one position to the right.\n\
+                \   wordLeft <TextEditor>             Move the cursor left to the nearest word.\n\
+                \   wordRight <TextEditor>            Move the cursor right to the nearest word.\n\
+                \   home <TextEditor>                 Move the cursor to the start of the line.\n\
+                \   end <TextEditor>                  Move the cursor to the end of the line.\n\
+                \   write <TextEditor>                Insert text at the current cursor position.\n\
+                \   selectLeft <TextEditor>           Highlight text one letter to the left of the cursor.\n\
+                \   selectRight <TextEditor>          Highlight text one letter to the right of the cursor.\n\
+                \   selectWordLeft <TextEditor>       Highlight the word to the left of the cursor.\n\
+                \   selectWordRight <TextEditor>      Highlight the word to the right of the cursor.\n\
+                \   selectAllLeft <TextEditor>        Highlight all text to the left of the cursor.\n\
+                \   selectAllRight <TextEditor>       Highlight all text to the right of the cursor.\n\
+                \   selectAll <TextEditor>            Highlight all text.\n\
+                \   copy <TextEditor>                 Copy the highlighted text to the clipboard.\n\
+                \   cut <TextEditor>                  Remove and copy the highlighted text to the clipboard.\n\
+                \   paste <TextEditor>                Insert text from the clipboard at the cursor position.\n\
+                \   backspace <TextEditor>            Remove character preceding the cursor.\n\
+                \   delete <TextEditor>               Remove character following the cursor.\n\
+                \   save <TextEditor> <File Path>     Save the text to the specified file path.\n\
+                \   open <File Path>                  Open the file in the text editor.\n\
+                \   concatenate <TextEditor>          Merge all text into a single word.\n\
+                \\n\
+                \With the parameters:\n\
+                \\n\
+                \   <TextEditor>                      A variable holding a TextEditor instance assigned by the \'create\' command.\n\
+                \   <Text>                            A sequence of alphanumeric characters.\n\
+                \   <File Path>                       A path to a text file (.txt).\n\
+                \\n\
+                \Example Usage:\n\
+                \\n\
+                \   a = create\n\
+                \   b = initialise a \"example text\"\n\
+                \   b\n\
+                \   > Line \"example text\" \"\" \"\" \"\"\n\
+                \   c = selectWordLeft b\n\
+                \   c\n\
+                \   > Line \"example\" \" text\" \"\" \"\"\n\
+                \   d = write c \" usage.\"\n\
+                \   > Line \"example usage.\" \"\" \"\" \"\"\n\
+                \" 
